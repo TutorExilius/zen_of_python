bd3dfb4a061abf4ed54d89324737858af5e804ee
Tutor Exilius <tutorexilius@gmail.com>
2022-03-25 23:14:56 +0100

    feat: add example for zen 8/9

--- zen_of_python/zen_08_and_09_examples.py
+++ zen_of_python/zen_08_and_09_examples.py
@@ -1,2 +1,23 @@
 """Zen 8: Special cases aren't special enough to break the rules."""
 """Zen 9: Although practicality beats purity."""
+
+
+# ------------ Example 1 ------------
+
+# enter a number and make sure it is a integer
+
+"""try to comply zen 7: 'Readability counts.'"""
+def enter_a_number() -> int:
+    while True:
+        number = input("Enter a number:")
+
+        if number.isdigit():
+            return int(number)
+
+"""break some ZENs, because of practicality"""
+def enter_a_number() -> int:
+    while True:
+        try:
+            return int(input("Enter a number:"))
+        except:
+            pass
